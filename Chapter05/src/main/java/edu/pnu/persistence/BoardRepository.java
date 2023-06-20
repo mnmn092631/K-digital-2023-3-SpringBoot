31f0b7be7033d0364b1ba4ff0c1845d37159aebd
juhee kang <mnmn092631@gmail.com>
2023-06-20 11:45:14 +0900

    Query 어노테이션

--- Chapter05/src/main/java/edu/pnu/persistence/BoardRepository.java
+++ Chapter05/src/main/java/edu/pnu/persistence/BoardRepository.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import edu.pnu.domain.Board;
 
@@ -21,4 +23,22 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
 	List<Board> findByCntBetweenOrderBySeqAsc(Long from, Long to);
 	
 	List<Board> findByTitleContainingOrContentContainingOrderBySeqDesc(String searchKeyword1, String searchKeyword2);
+	
+	@Query("SELECT b FROM Board b WHERE b.title LIKE %?1% ORDER BY b.seq DESC")
+	List<Board> queryAnnotationTest1(String searchKeyword);
+	
+	@Query("SELECT b FROM Board b WHERE b.title LIKE %:searchKeyword% ORDER BY b.seq DESC")
+	List<Board> queryAnnotationTest2(String searchKeyword);
+	
+	@Query("SELECT b.seq, b.title, b.writer, b.createDate FROM Board b "
+			+ "WHERE b.title LIKE %:searchKeyword% ORDER BY b.seq DESC")
+	List<Object[]> queryAnnotationTest3(String searchKeyword);
+	
+	@Query(value="SELECT seq, title, writer, create_date "
+			+ "FROM board WHERE title LIKE '%'||:sk||'%' "
+			+ "ORDER BY seq DESC", nativeQuery = true)
+	List<Object[]> queryAnnotationTest4(@Param("sk") String searchKeyword);
+	
+	@Query("SELECT b FROM Board b")
+	List<Board> queryAnnotationTest5(Pageable paging);
 }
--- Chapter05/src/test/java/edu/pnu/BoardRepositoryTest.java
+++ Chapter05/src/test/java/edu/pnu/BoardRepositoryTest.java
@@ -1,5 +1,6 @@
 package edu.pnu;
 
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -9,6 +10,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import edu.pnu.domain.Board;
 import edu.pnu.persistence.BoardRepository;
@@ -28,7 +32,7 @@ class BoardRepositoryTest {
 		}
 	}
 
-	@Test
+//	@Test
 	public void testGetBoard() {
 //		Optional<Board> opt = boardRepo.findById(1L);
 //		Board board = opt.get();
@@ -37,7 +41,7 @@ class BoardRepositoryTest {
 		System.out.println(board);
 	}
 
-	@Test
+//	@Test
 	public void testUpdateBoard() {
 //		System.out.println("=== 1번 게시글 조회 ===");
 //		Board board = boardRepo.findById(1L).get();
@@ -65,10 +69,53 @@ class BoardRepositoryTest {
 		boardRepo.deleteById(2L);
 	}
 
-	@Test
+//	@Test
 	public void testFindAll() {
 		List<Board> list = boardRepo.findAll();
 		System.out.println("모든 데이터를 출력합니다.");
+		for (Board b : list) {
+			System.out.println(b);
+		}
+	}
+
+//	@Test
+	public void testQueryAnnotationTest1() {
+		List<Board> list = boardRepo.queryAnnotationTest1("title1");
+		for (Board b : list) {
+			System.out.println(b);
+		}
+	}
+
+//	@Test
+	public void testQueryAnnotationTest2() {
+		List<Board> list = boardRepo.queryAnnotationTest2("title1");
+		for (Board b : list) {
+			System.out.println(b);
+		}
+	}
+
+//	@Test
+	public void testQueryAnnotationTest3() {
+		List<Object[]> list = boardRepo.queryAnnotationTest3("title1");
+		for (Object[] b : list) {
+			System.out.println(Arrays.toString(b));
+		}
+	}
+
+//	@Test
+	public void testQueryAnnotationTest4() {
+		List<Object[]> list = boardRepo.queryAnnotationTest4("title1");
+		for (Object[] b : list) {
+			System.out.println(Arrays.toString(b));
+		}
+	}
+
+	@Test
+	public void testQueryAnnotationTest5() {
+//		Pageable paging = PageRequest.of(0, 3, Sort.Direction.DESC, "seq");
+		Pageable paging = PageRequest.of(5, 5);
+		List<Board> list = boardRepo.queryAnnotationTest5(paging);
+
 		for (Board b : list) {
 			System.out.println(b);
 		}
